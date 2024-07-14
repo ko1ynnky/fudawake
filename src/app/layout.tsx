f83c6257ee612ece00721bff63fa2f824911a50d
ko1ynnky <cyowwwoyakitawwwotz@gmail.com>
2024-07-14 12:54:57 +0900

    metadata追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 'use client';
 
 import { ChakraProvider } from '@chakra-ui/react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '札分けアプリ',
+  description: '競技かるたの練習で、札分け番号を自動で決めてくれます',
+};
 
 export default function RootLayout({
   children,
